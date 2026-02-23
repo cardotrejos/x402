487dbcf93858a90a499c129b34653202646cdc7a
Ricardo Trejos <cardotrejos@gmail.com>
2026-02-22 19:55:17 -0500

    fix: add moduledoc and @doc since annotation to X402.Behaviour
    Addresses Greptile review comments:
    - Replace @moduledoc false with descriptive module documentation
    - Add @doc since: "0.4.0" annotation to implements?/2 function

--- lib/x402/behaviour.ex
+++ lib/x402/behaviour.ex
@@ -1,6 +1,12 @@
 defmodule X402.Behaviour do
-  @moduledoc false
+  @moduledoc """
+  Utility functions for checking behaviour implementations.
 
+  Used internally to verify that modules implement the required
+  callbacks for x402 extension behaviours.
+  """
+
+  @doc since: "0.4.0"
   @doc """
   Checks if a module implements a set of callbacks.
   """
